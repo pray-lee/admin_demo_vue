a32cbcf63e896ab6fbad739c4d260c2692c6f0c0
pray-lee <praycis@gmail.com>
2019-07-08 14:49:59 +0800

    completed paginations

--- node-admin-ele-app/client/src/components/FundList.vue
+++ node-admin-ele-app/client/src/components/FundList.vue
@@ -90,6 +90,20 @@
           </template>
         </el-table-column>
       </el-table>
+      <el-row>
+        <el-col :span='24'>
+          <el-pagination
+            class="pagination-wrapper"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="paginations.currentPage"
+            :page-sizes="paginations.pageSizes"
+            :page-size="paginations.pageSize"
+            :layout="paginations.layout"
+            :total="paginations.total">
+          </el-pagination>
+        </el-col>
+      </el-row>
     </div>
     <div class="dialog-container">
       <addDialog :dialogStatus="dialogStatus" :form="form" @finished="updateProfile"></addDialog>
@@ -107,7 +121,15 @@
     },
     data() {
       return {
-        list: [],
+        paginations: {
+          currentPage: 1,
+          pageSize: 5,
+          pageSizes: [5, 10, 15, 20],
+          layout: "total, sizes, prev, pager, next, jumper",
+          total: 0
+        },
+        list: [], // 每页显示的数据
+        allTableList: [], // 总的数据
         dialogStatus: {
           show: false,
           title: '',
@@ -127,15 +149,45 @@
       this.getAllProfiles()
     },
     methods: {
+      handleSizeChange (val) {
+        this.paginations.currentPage = 1
+        this.paginations.pageSize = val
+        this.list = this.allTableList.filter((item, index) => index < val)
+      },
+      handleCurrentChange (val) {
+        let idx = this.paginations.pageSize * (val - 1) // 到上一页的所有数据
+        let currentIdx = this.paginations.pageSize * val // 到目标页的所有数据
+        let table = []
+        for (let i = idx; i < currentIdx; i++) { // 两个数据之间的部分遍历，得到的就是目标页显示的数据
+          if (!!this.allTableList[i])
+            table.push(this.allTableList[i]) 
+        }
+        this.list = table
+      },
+      setPaginations () {
+        this.paginations.total = this.allTableList.length
+        this.list = this.allTableList.filter((item, index) => index < this.paginations.pageSize)
+      },
       getAllProfiles () {
         getProfiles()
           .then(res => {
             if (res.status === 200) {
-              this.list = res.data
+              this.allTableList = res.data
+              // set paginations
+              this.setPaginations()
             }
           })
       },
       handleAdd () {
+        this.form = {
+         remark: '',
+          cash: '',
+          income: '',
+          expend: '',
+          describe: '',
+          type: '',
+          id: ''
+        }
         this.dialogStatus = {
           show: true,
           title: '添加资产',
@@ -182,4 +234,6 @@
     .button-wrapper
       float:right
       margin-right:60px
+    .pagination-wrapper
+      text-align: right
 </style>
